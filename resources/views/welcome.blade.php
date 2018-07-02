d11c9f874140cf8a3bb80ddd906fa815e4f701f2
adsants <mail.adisantoso@gmail.com>
2018-07-03 01:46:51 +0700

    joss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		@include ('head')  
+	</head>
+	<body class="hold-transition skin-blue sidebar-mini">
+		<div class="wrapper">
+			@include ('header')  
+			@include ('sidebar')  
+			@include('content')
+			@include ('footer')    
+		</div><!-- ./wrapper -->
+		@include ('script')     
+	</body>
+</html>
